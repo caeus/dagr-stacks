ae9eb7e2eed74ba251f7a8c5af45f5a7a6f72165
caeus <camilo.a.navas@gmail.com>
2026-09-01 18:32:13 +0200

    Use published DI stack image (#14)

--- typescript/di/dagr.index.js
+++ typescript/di/dagr.index.js
@@ -1,24 +1,7 @@
-const DI_COMMIT = 'a8782212bc94065dff632749f884ff84022d314e'
-
 export default {
   '/': {
-    FROM: 'alpine:3.22',
-    steps: [
-      { RUN: 'apk add --no-cache git' },
-      {
-        RUN: [
-          'git init /src',
-          'cd /src',
-          'git remote add origin https://github.com/caeus/dagr-stacks.git',
-          'git sparse-checkout init --cone',
-          'git sparse-checkout set di',
-          `git fetch --depth=1 --filter=blob:none origin ${DI_COMMIT}`,
-          'git checkout --detach FETCH_HEAD',
-          'rm -f /src/di/dagr.di.test.js /src/di/package.json',
-        ].join(' && '),
-      },
-      { WORKDIR: '/src/di' },
-    ],
+    FROM: 'ghcr.io/caeus/dagr-stacks-di:7de90f567348a68cdbff0968757d7f49096b1aab',
+    steps: [],
     IGNORE: [],
   },
 }
